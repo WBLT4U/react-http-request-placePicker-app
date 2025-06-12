1ef5365eb86cbbefa0d95615d34dd3f6916bcea7
Taofiq Abdulsalam <taofiqabdulsalam48@gmail.com>
2025-06-12 18:03:32 +0100

    port refactor

--- backend/app.js
+++ backend/app.js
@@ -50,4 +50,7 @@ app.use((req, res, next) => {
   res.status(404).json({ message: '404 - Not Found' });
 });
 
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
