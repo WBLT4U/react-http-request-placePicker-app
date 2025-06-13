89db32c7edfde4c59f98df5bb027bc00589c59d5
Taofiq Abdulsalam <taofiqabdulsalam48@gmail.com>
2025-06-13 08:56:11 +0100

    place Render api

--- src/components/Places.jsx
+++ src/components/Places.jsx
@@ -18,7 +18,7 @@ export default function Places({
             <li key={place.id} className="place-item">
               <button onClick={() => onSelectPlace(place)}>
                 <img
-                  src={`http://localhost:3000/${place.image.src}`}
+                  src={`https://react-http-request-placepickers-app.onrender.com/${place.image.src}`}
                   alt={place.image.alt}
                 />
                 <h3>{place.title}</h3>
