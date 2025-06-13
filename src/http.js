166ee634007f061a0ed92b157530e37d799a3f66
Taofiq Abdulsalam <taofiqabdulsalam48@gmail.com>
2025-06-13 09:08:26 +0100

    refactor Render api

--- src/http.js
+++ src/http.js
@@ -10,7 +10,7 @@ export async function fetchAvailablePlaces() {
 }
 
 export async function fetchUserPlaces() {
-  const response = await fetch('https://react-http-request-placepickers-app.onrender.com/places');
+  const response = await fetch('https://react-http-request-placepickers-app.onrender.com/user-places');
   const resData = await response.json();
 
   if (!response.ok) {
@@ -21,7 +21,7 @@ export async function fetchUserPlaces() {
 }
 
 export async function updateUserPlaces(places) {
-  const response = await fetch('https://react-http-request-placepickers-app.onrender.com/places', {
+  const response = await fetch('https://react-http-request-placepickers-app.onrender.com/user-places', {
     method: 'PUT',
     body: JSON.stringify({ places }),
     headers: {
